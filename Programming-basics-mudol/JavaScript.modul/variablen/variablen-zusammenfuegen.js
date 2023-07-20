c4bb6e31958404f55f2de9c0dfb0d0745e6beaf7
mdhyan.99@gmail.com <mdhyan.99@gmail.com>
2023-07-20 17:02:10 +0200

    bac

--- Programming-basics-mudol/JavaScript.modul/variablen/variablen-zusammenfuegen.js
+++
@@ -1,7 +0,0 @@
-const essen1 = "pizza";
-const essen2 = "döner";
-const essen3 = "pommes";
-const muhannadessen = essen1
-console.log("muhannad essen:", muhannadessen )
-const munseessen = essen1 +" und " + essen3
-console.log("muhannad essen:", munseessen  )
